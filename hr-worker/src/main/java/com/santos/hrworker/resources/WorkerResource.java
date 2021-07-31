547541bd003f151e9fdc373abc954d6c6337464b
isaiassantossilva <isaias661248@gmail.com>
2021-07-31 02:34:20 -0300

    add ribbon

--- hr-worker/src/main/java/com/santos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/santos/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.santos.hrworker.resources;
 
 import com.santos.hrworker.entities.Worker;
 import com.santos.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,11 @@ import java.util.List;
 @RequestMapping("/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment environment;
+
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -24,6 +32,7 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public Worker findById(@PathVariable long id){
+        logger.info("POST = " + environment.getProperty("local.server.port"));
         return workerRepository.findById(id).orElseThrow(() -> new RuntimeException("Worker not found"));
     }
 }
