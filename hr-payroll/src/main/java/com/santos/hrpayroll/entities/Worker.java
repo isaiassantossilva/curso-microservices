d34c720dd56bdaf64c5270fafb5a52cf3aa2a51e
isaiassantossilva <isaias661248@gmail.com>
2021-07-31 01:07:15 -0300

    communication between services

---
+++ hr-payroll/src/main/java/com/santos/hrpayroll/entities/Worker.java
@@ -0,0 +1,56 @@
+package com.santos.hrpayroll.entities;
+
+import java.util.Objects;
+
+public class Worker {
+
+    private long id;
+    private String name;
+    private double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return id == worker.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
